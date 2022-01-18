baffd83480757b53f8adb33c4ff8c2b410850317
Jens Müller <jens.mueller@de.ibm.com>
2022-01-18 13:15:00 +0100

    Fix copyright headers

--- cpo/utils/string.py
+++ cpo/utils/string.py
@@ -1,4 +1,4 @@
-#  Copyright 2021 IBM Corporation
+#  Copyright 2021, 2022 IBM Corporation
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
@@ -11,6 +11,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+
 
 def removeprefix(str: str, prefix: str) -> str:
     """Returns the given string without the given prefix if it starts with
