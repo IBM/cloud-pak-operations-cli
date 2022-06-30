ef377394cb68b8a10c27b780c9c2cc65f6768c63
Jakob-Waibel <jakob.waibel@ibm.com>
2022-06-30 09:55:45 +0200

    feat: Add commands to install and list dev-plugins

---
+++ cpo/commands/adm/dev-plugin/install.py
@@ -0,0 +1,27 @@
+#  Copyright 2022 IBM Corporation
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import click
+
+import cpo.lib.dev_plugin.install
+
+from cpo.utils.logging import loglevel_command
+
+
+@loglevel_command()
+@click.argument("module")
+def install(module: str):
+    """Installs dev-plugin"""
+
+    cpo.lib.dev_plugin.install.install(module)
---
+++ cpo/lib/dev_plugin/list.py
@@ -0,0 +1,30 @@
+#  Copyright 2022 IBM Corporation
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+# Install latest version - we need to crawl the files
+import cpo.utils.process
+
+
+def list():
+    """Lists all dev-plugins"""
+
+    cpo.utils.process.execute_command(
+        "pip",  # type: ignore
+        [
+            "search",
+            "-i",
+            "https://na.artifactory.swg-devops.com/artifactory/api/pypi/idaa-data-gate-cli-pypi-local/simple",
+            "cloud-pak-operations-cli"
+        ],
+    )
