31bd469c1732f14d174fab6bf3e88a4797b35bbe
Jakob-Waibel <jakob.waibel@ibm.com>
2022-07-01 07:49:02 +0200

    refactor: Remove outdated commands

--- tests/test/commands/ibmcloud/test_ibmcloud_common.py
+++ tests/test/commands/ibmcloud/test_ibmcloud_common.py
@@ -18,9 +18,7 @@ from pathlib import Path
 from unittest.mock import patch
 
 from cpo.lib.ibmcloud.iam import api_key_exists, generate_api_key
-from cpo.lib.ibmcloud.install import _get_cp4d_version_locator
 from cpo.lib.ibmcloud.status import get_cluster_status
-from cpo.lib.ibmcloud.vlan_manager import VLANManager
 from cpo.utils.process import ProcessResult
 
 
@@ -51,32 +49,6 @@ class TestIBMCloudCommon(unittest.TestCase):
         self.assertFalse(api_key_exists("cpo.api.key.3"))
 
     @patch(
-        "cpo.lib.ibmcloud.vlan_manager.execute_ibmcloud_command",
-        return_value=ProcessResult(
-            stderr="",
-            stdout=(Path(__file__).parent / "dependencies/ibmcloud_list_vlans.json").read_text(),
-            return_code=0,
-        ),
-    )
-    def test_get_default_public_vlan(self, test_mock):
-        vlan_manager = VLANManager("sjc03")
-
-        self.assertEqual("2734440", vlan_manager.default_public_vlan)
-
-    @patch(
-        "cpo.lib.ibmcloud.vlan_manager.execute_ibmcloud_command",
-        return_value=ProcessResult(
-            stderr="",
-            stdout=(Path(__file__).parent / "dependencies/ibmcloud_list_vlans.json").read_text(),
-            return_code=0,
-        ),
-    )
-    def test_get_default_private_vlan(self, test_mock):
-        vlan_manager = VLANManager("sjc03")
-
-        self.assertEqual("2734442", vlan_manager.default_private_vlan)
-
-    @patch(
         "cpo.lib.ibmcloud.status.execute_ibmcloud_command",
         return_value=ProcessResult(
             stderr="",
@@ -97,20 +69,6 @@ class TestIBMCloudCommon(unittest.TestCase):
     )
     def test_is_cluster_ready_true(self, test_mock):
         self.assertTrue(get_cluster_status("datagate.test").is_ready())
-
-    @patch(
-        "cpo.lib.ibmcloud.install.execute_ibmcloud_command",
-        return_value=ProcessResult(
-            stderr="",
-            stdout=Path(Path(__file__).parent / "dependencies/ibmcloud_catalog_search_pak.json").read_text(),
-            return_code=0,
-        ),
-    )
-    def test_get_cp4d_version_locator(self, test_mock):
-        self.assertEqual(
-            _get_cp4d_version_locator("3.0.1"),
-            "1082e7d2-5e2f-0a11-a3bc-f88a8e1931fc.bac1d34a-d912-45e4-bc81-10cd7b94bcc1-global",
-        )
 
 
 if __name__ == "__main__":
