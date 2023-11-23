ca758459f653c62cf6e708f244f59bd20627720b
Jens Müller <jens.mueller@de.ibm.com>
2023-11-23 16:15:00 +0100

    Improve management of internal plug-ins
    Signed-off-by: Jens Müller <jens.mueller@de.ibm.com>

--- cpo/commands/adm/ibm_internal_plugin/install.py
+++ cpo/commands/adm/ibm_internal_plugin/install.py
@@ -12,13 +12,20 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+import importlib.metadata
+import logging
+
 import click
+import semver
 
 import cpo.config.cluster_credentials_manager
 import cpo.lib.click.utils
 
+from cpo.lib.click.utils import get_semver_version
 from cpo.lib.ibm_internal_plugin.ibm_internal_plugin_manager import IBMInternalPluginInstaller
 from cpo.utils.logging import loglevel_command
+
+logger = logging.getLogger(__name__)
 
 
 @loglevel_command(
@@ -41,6 +48,7 @@ from cpo.utils.logging import loglevel_command
     default="https://na.artifactory.swg-devops.com/artifactory/api/pypi/hyc-ibm-sap-cp4d-team-pypi-local/simple",
 )
 @click.option("--user", help="Install to user site-packages directory", is_flag=True)
+@click.option("--version", callback=get_semver_version, help="Plug-in version")
 @click.argument("distribution-package-names", nargs=-1)
 def install(
     artifactory_username: str,
@@ -48,10 +56,46 @@ def install(
     distribution_package_names: list[str],
     repository_url: str,
     user: bool,
+    version: semver.Version | None,
 ):
     """Install IBM-internal CLI plug-ins"""
 
     for distribution_package_name in distribution_package_names:
+        kwargs = {"user": user, "version": version}
+
+        if version is not None:
+            if (
+                local_distribution_package_version := _get_distribution_package_version(distribution_package_name)
+            ) is not None:
+                if version == local_distribution_package_version:
+                    logger.info(f"Plug-in '{distribution_package_name}' (version: {version}) is already installed")
+
+                    continue
+
+                if version > local_distribution_package_version:
+                    logger.info(
+                        f"Upgrading plug-in '{distribution_package_name}' from version "
+                        f"{local_distribution_package_version} to {version}"
+                    )
+                else:
+                    logger.info(
+                        f"Downgrading plug-in '{distribution_package_name}' from version "
+                        f"{local_distribution_package_version} to {version}"
+                    )
+
+                kwargs["upgrade"] = True
+
         IBMInternalPluginInstaller(artifactory_username, artifactory_password, repository_url).install(
-            distribution_package_name, user
+            distribution_package_name, **kwargs
         )
+
+
+def _get_distribution_package_version(distribution_package_name: str) -> semver.Version | None:
+    distribution_package_version: semver.Version | None = None
+
+    try:
+        distribution_package_version = semver.Version.parse(importlib.metadata.version(distribution_package_name))
+    except importlib.metadata.PackageNotFoundError:
+        pass
+
+    return distribution_package_version
