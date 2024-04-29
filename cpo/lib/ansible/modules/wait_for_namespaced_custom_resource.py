c8e249bcafd6e96859c5af3c29e9baec92a563ab
Jens Müller <jens.mueller@de.ibm.com>
2024-04-29 10:00:00 +0200

    OpenShiftAPIManager: Log exception when waiting for namespaced custom resource
    Signed-off-by: Jens Müller <jens.mueller@de.ibm.com>

--- cpo/lib/ansible/modules/wait_for_namespaced_custom_resource.py
+++ cpo/lib/ansible/modules/wait_for_namespaced_custom_resource.py
@@ -1,4 +1,4 @@
-#  Copyright 2022, 2023 IBM Corporation
+#  Copyright 2022, 2024 IBM Corporation
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
@@ -11,6 +11,8 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+
+import logging
 
 from dataclasses import dataclass
 from typing import Any
@@ -100,7 +102,9 @@ class WaitForNamespacedCustomResourceModule(AbstractModule):
             )
 
             result = {"changed": False}
-        except Exception:
+        except Exception as exception:
+            self._log(logging.ERROR, str(exception))
+
             result = {"changed": False, "failed": True}
 
         self._module.exit_json(**result)
